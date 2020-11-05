a04c4eb601d50ed24db2886372fe73a6599711be
Yuto Hayashi <hyasssy@gmail.com>
2020-11-05 20:15:05 +0900

    内容加筆修正

--- docs/js/template.js
+++ docs/js/template.js
@@ -38,9 +38,9 @@ function header(type) {
 function copyright() {
     var html = "";
     html += '<div class="copyright">';
-    html += '<span>©️ 2020, Yuto Hayashi - </span>';
-    html += '<a href="https://www.instagram.com/hyasssy" target="_blank" >Instagram </a>';
-    html += '<a href="https://twitter.com/hyasssy" target="_blank" >Twitter</a>';
+    html += '<span>©️ 2020, Yuto Hayashi</span>';
+    //html += '<a href="https://www.instagram.com/hyasssy" target="_blank" >Instagram </a>';
+    //html += '<a href="https://twitter.com/hyasssy" target="_blank" >Twitter</a>';
     html += '</div>';
     document.write(html);
 }
