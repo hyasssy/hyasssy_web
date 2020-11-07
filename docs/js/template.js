d0ae4cbda08321d4127dd3eec6ebfd982b6c3243
Yuto Hayashi <hyasssy@gmail.com>
2020-11-08 00:36:24 +0900

    枠組み完成

--- docs/js/template.js
+++ docs/js/template.js
@@ -1,36 +1,69 @@
 // ヘッダーとコピーライトのテンプレート
 
-var url_works = 'works/y2020_caniseeyounow.html';//worksページのトップ作品URL
-
 function header(type) {
     var html = "";
+    var path = type == 'top' ? '' : '../';
+    html += '<div id="nav-container">';
+    html += '<input id="nav-input" type="checkbox" class="nav-unshown">'
+    html += '<label id="nav-open" for="nav-input">'
+    html += '<img src="' + path + 'img/icon/icon_navi.png" alt="icon">';
+    html += '</label>';
+    html += '<label class="nav-unshown" id="nav-close" for="nav-input"></label>';
+    html += '<div id="nav-contents">';
+    html += '<div id="nav-wrapper">';
+    html += '<a href="' + path + 'news/index.html" class="nav-title">News</a>';
+    html += '<a href="' + path + 'works/index.html" class="nav-title">Works</a>';
+    html += '<a href="' + path + 'texts/index.html" class="nav-title">Texts</a>';
+    html += '<a href="' + path + 'cv/index.html" class="nav-title">CV</a>';
+    html += '<a href="' + path + 'contact/index.html" class="nav-title">Contact?</a>';
+    html += '<div class="nav-snsicon"><a icon" href="https://www.instagram.com/hyasssy" target="_blank" ><img src="' + path + 'img/icon/icon_insta.png" alt="icon"></a>';
+    html += '<a icon" href="https://twitter.com/hyasssy" target="_blank" ><img src="' + path + 'img/icon/icon_twitter.png" alt="icon"></a></div>';
+    html += '</div>';//nav-wrapper
+    html += '</div>';//nav-contents
+    html += '</div>';//nav-container
     html += '<div class="header">';
     if (type == 'top') {
         html += '<div>Yuto&nbsp;Hayashi</div>';
         html += '<div class="header-list-container">';
-        html += '<a class ="header-list" href="' + url_works + '">works</a>';
         html += '<a class ="header-list" href="news/index.html">news</a>';
-        html += '<a class ="header-list" href="cv-contact/index.html">cv&contact?</a>';
+        html += '<a class ="header-list" href="works/index.html">works</a>';
+        html += '<a class ="header-list" href="texts/index.html">texts</a>';
+        html += '<a class ="header-list" href="cv/index.html">cv</a>';
+        html += '<a class ="header-list" href="contact/index.html">contact?</a>';
+        html += '</div>';//header-list-container
     } else {
         html += '<a href="../index.html">Yuto&nbsp;Hayashi</a>';
         html += '<div class="header-list-container">';
-        if (type == 'works') {
-            html += '<div class ="header-list">WORKS</div>';
-        } else {
-            html += '<a class ="header-list" href="../' + url_works + '">works</a>';
-        }
         if (type == 'news') {
             html += '<div class ="header-list">NEWS</div>';
         } else {
             html += '<a class ="header-list" href="../news/index.html">news</a>';
         }
-        if (type == 'cv-contact') {
-            html += '<div class ="header-list">CV&CONTACT?</div>';
-        } else {
-            html += '<a class ="header-list" href="../cv-contact/index.html">cv&contact?</a>';
+        if (type == 'works') {
+            html += '<div class ="header-list">WORKS</div>';
+        } else if (type == 'works-contents') {
+            html += '<a class ="header-list" href="../works/index.html">WORKS</a>';
         }
+        else {
+            html += '<a class ="header-list" href="../works/index.html">works</a>';
+        }
+        if (type == 'texts') {
+            html += '<div class ="header-list">TEXTS</div>';
+        } else {
+            html += '<a class ="header-list" href="../texts/index.html">texts</a>';
+        }
+        if (type == 'cv') {
+            html += '<div class ="header-list">CV</div>';
+        } else {
+            html += '<a class ="header-list" href="../cv/index.html">cv</a>';
+        }
+        if (type == 'contact') {
+            html += '<div class ="header-list">CONTACT?</div>';
+        } else {
+            html += '<a class ="header-list" href="../contact/index.html">contact?</a>';
+        }
+        html += '</div>';//header-list-container
     }
-    html += '</div>';
     html += '</div>';
     document.write(html);
 }
--- docs/js/works_template.js
+++ docs/js/works_template.js
@@ -1,37 +1,83 @@
-function thumbnail(link, imgsrc, captext, filename, mediatype) {
+function thumbnail(number, link, imgsrc, captext, isonpage, mediatype) {
+    var style = "";
+    if (isonpage) {
+        style = 'style="opacity:1"';//当該ページの場合は半透明じゃなくす。
+    }
     var html = "";
-    html += '<div class="square-frame">';
-    html += '<div class="thumbnail-media">' + mediatype + '</div>'
-    html += filename == link ? '<div>' : '<a href="' + link + '">';//同じページにリンクしないようにする処置
+    switch (number) {//いずれshowリストも追加する。
+        case 16:
+            html += '<div class="thumbnail-border"><div>▼Portfolio</div></div>';
+            break;
+        case 15:
+            html += '<div class="thumbnail-space"></div>';
+            html += '<div class="thumbnail-border"><div>▼2020</div></div>';
+            break;
+        case 9:
+            html += '<div class="thumbnail-space"></div>';
+            html += '<div class="thumbnail-border"><div>▼2019</div></div>';
+            break;
+        case 3:
+            html += '<div class="thumbnail-space"></div>';
+            html += '<div class="thumbnail-border"><div>▼~2018</div></div>';
+            break;
+        default: break;
+    }
+    html += '<div class="square-frame" ' + style + '>';
+    html += '<div class="thumbnail-media" ' + style + '>' + mediatype + '</div>'
+    html += isonpage ? '<span>' : '<a href="' + link + '">';//同じページにリンクしないようにする処置
     html += '<div class="square-content" style="background-image: url(' + imgsrc + ');"></div>';
-    html += filename == link ? '</div>' : '</a >';
-    html += '<div class="thumbnail-caption">' + captext + '</div>';
+    html += isonpage ? '</span>' : '</a >';
+    html += '<div class="thumbnail-caption" ' + style + '>' + captext + '</div>';
     html += '</div>';
     document.write(html);
 }
-function workthumbnail(filename) {
-    var html = "";
-    html += '<div class="thumbnail-border"><div>▼Works&nbsp;List</div></div>'
-    document.write(html);
-    thumbnail('y2020_caniseeyounow.html', '../img/y2020_caniseeyounow/img4.jpg', 'Can I see You now?', filename, 'online app');
-    thumbnail('y2020_iwanttogetclosemore.html', '../img/y2020_iwanttogetclosemore/thumbnail.jpg', 'I want to get close more...', filename, 'video');
-    thumbnail('y2020_isyourbodyyours.html', '../img/y2020_isyourbodyyours/img4.jpg', 'Is your body yours?', filename, 'VR installation');
-    thumbnail('y2019_practicetowalk.html', '../img/y2019_practicetowalk/img4.jpg', 'practice to walk', filename, 'video');
-    thumbnail('y2019_followingthesun.html', '../img/y2019_followingthesun/img1.jpg', 'Following the Sun', filename, 'video');
-    thumbnail('y2019_sowhoami.html', '../img/y2019_sowhoami/img1.jpg', 'So, who am I?', filename, 'VR installation');
-    thumbnail('y2019_osorubeki.html', '../img/y2019_osorubeki/img1.jpg', '恐るべき子供達', filename, 'simulation');
-    thumbnail('y2018_japanesefaces.html', '../img/y2018_japanesefaces/img1.jpg', '日本人風之図', filename, 'installation');
-    thumbnail('y2017_secretplace.html', '../img/y2017_secretplace/img1.jpg', 'secret place', filename, 'installation');
-    thumbnail('y2015_omatsurisawagi.html', '../img/y2015_omatsurisawagi/img1.jpg', '御祭騒ぎ', filename, 'flat work');
-    var html = "";
-    html += '<div class="thumbnail-space"></div>';
-    html += '<div class="thumbnail-border"><div>▼Small&nbsp;Works</div></div>'
-
-    document.write(html);
-    thumbnail('y2020_lumiererule.html', '../img/y2020_lumiererule/thumbnail.jpg', 'Lumiere rule', filename, 'video');
-    thumbnail('y2020_followingthesun_photo.html', '../img/y2020_followingthesun_photo/img1.jpg', 'Following the Sun', filename, 'photo');
-    thumbnail('y2020_thumbnail.html', '../img/y2020_koko/img1.jpg', 'ここ', filename, 'video');
-    thumbnail('y2019_selfportrait.html', '../img/y2019_selfportrait/img1.jpg', 'Self-portrait', filename, 'flatwork');
-    thumbnail('y2019_eupcaccia.html', '../img/y2019_eupcaccia/img3.jpg', 'Eupcaccia', filename, 'simulation');
-    thumbnail('y2017_paint.html', '../img/y2017_paint/img1.jpg', 'flat works', filename, 'flat work');
+function thumbnaillist(number, pagenumber) {
+    switch (number) {
+        case 16: thumbnail(number, 'portfolio.pdf", target="_blank', '../img/thumbnail/t_portfolio.jpg', '2020 Mar.', pagenumber == number, '');
+            break;
+        case 15: thumbnail(number, 'y2020_caniseeyounow.html', '../img/thumbnail/t_caniseeyounow.jpg', 'Can I see You now?', pagenumber == number, 'online app');
+            break;
+        case 14: thumbnail(number, 'y2020_iwanttogetclosemore.html', '../img/thumbnail/t_iwanttogetclosemore.jpg', 'I want to get close more...', pagenumber == number, 'video');
+            break;
+        case 13: thumbnail(number, 'y2020_lumiererule.html', '../img/thumbnail/t_lumiererule.jpg', 'Lumiere rule', pagenumber == number, 'video');
+            break;
+        case 12: thumbnail(number, 'y2020_followingthesun_photo.html', '../img/thumbnail/t_followingthesun_photo.jpg', 'Following the Sun', pagenumber == number, 'photo');
+            break;
+        case 11: thumbnail(number, 'y2020_thumbnail.html', '../img/thumbnail/t_koko.jpg', 'ここ', pagenumber == number, 'video');
+            break;
+        case 10: thumbnail(number, 'y2020_isyourbodyyours.html', '../img/thumbnail/t_isyourbodyyours.jpg', 'Is your body yours?', pagenumber == number, 'VR installation');
+            break;
+        case 9: thumbnail(number, 'y2019_selfportrait.html', '../img/thumbnail/t_selfportrait.jpg', 'Self-portrait', pagenumber == number, 'flat work');
+            break;
+        case 8: thumbnail(number, 'y2019_practicetowalk.html', '../img/thumbnail/t_practicetowalk.jpg', 'practice to walk', pagenumber == number, 'video');
+            break;
+        case 7: thumbnail(number, 'y2019_followingthesun.html', '../img/thumbnail/t_followingthesun.jpg', 'Following the Sun', pagenumber == number, 'video');
+            break;
+        case 6: thumbnail(number, 'y2019_sowhoami.html', '../img/thumbnail/t_sowhoami.jpg', 'So, who am I?', pagenumber == number, 'VR installation');
+            break;
+        case 5: thumbnail(number, 'y2019_eupcaccia.html', '../img/thumbnail/t_eupcaccia.jpg', 'Eupcaccia', pagenumber == number, 'simulation');
+            break;
+        case 4: thumbnail(number, 'y2019_osorubeki.html', '../img/thumbnail/t_osorubeki.jpg', '恐るべき子供達', pagenumber == number, 'simulation');
+            break;
+        case 3: thumbnail(number, 'y2018_japanesefaces.html', '../img/thumbnail/t_japanesefaces.jpg', '日本人風之図', pagenumber == number, 'installation');
+            break;
+        case 2: thumbnail(number, 'y2017_secretplace.html', '../img/thumbnail/t_secretplace.jpg', 'secret place', pagenumber == number, 'installation');
+            break;
+        case 1: thumbnail(number, 'y2017_paint.html', '../img/thumbnail/t_paint.jpg', 'flat works', pagenumber == number, 'flat work');
+            break;
+        case 0: thumbnail(number, 'y2015_omatsurisawagi.html', '../img/thumbnail/t_omatsurisawagi.jpg', '御祭騒ぎ', pagenumber == number, 'flat work');
+            break;
+        default: break;
+    }
+}
+function workthumbnailtop(pagenumber) {
+    for (var i = 16; i > 0; i--) {
+        thumbnaillist(i, pagenumber);
+        if (pagenumber == i) return;
+    }
+}
+function workthumbnailbottom(pagenumber) {
+    for (var i = 16; i >= 0; i--) {
+        if (pagenumber > i) thumbnaillist(i, pagenumber);
+    }
 }
