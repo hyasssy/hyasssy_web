84a694dab334681899c6ffa064fc3760083beb8e
Yuto Hayashi <hyasssy@gmail.com>
2020-11-25 18:46:20 +0900

    web更新

--- docs/js/template.js
+++ docs/js/template.js
@@ -77,3 +77,24 @@ function copyright() {
     html += '</div>';
     document.write(html);
 }
+
+function langswitchbutton(){
+    var html = "";
+    html += '';
+    document.write(html);
+}
+
+//言語切り替え
+function langSet(argLang) {
+    // --- 切り替え対象のclass一覧を取得 ----------------------
+    var elm = document.getElementsByClassName("langCng");
+    for (var i = 0; i < elm.length; i++) {
+    // --- 選択された言語と一致は表示、その他は非表示 -------
+    if (elm[i].getAttribute("lang") == argLang) {
+      elm[i].style.display = '';
+    }
+    else {
+        elm[i].style.display = 'none';
+    }
+  }
+}
