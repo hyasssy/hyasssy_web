7a553553d5f97b05e5e31dccbc388a08b3e31662
Yuto Hayashi <hyasssy@gmail.com>
2021-06-06 02:59:02 +0900

    データの整理、軽微な修正、情報流し込み中

--- docs/project/renewal/js/works_data.js
+++ docs/project/renewal/js/works_data.js
@@ -1,65 +1,277 @@
+/*
+Format
+
 const _thumbnaildata = [
     {
-        root_id: "testid1",
-        img_url: "./img/exhidm.jpg",
-        title_id: "title1"
+        root_id: "hoge",
+        img_url: "./img/thumbnails/t_hoge.jpg"
+    }
+]
+const _documentdata = {
+    //ここのキーは_thumbnaildataのroot_idと同じにする。
+    hoge: {
+        links: [{ id: "link_hoge", url: "hoge" }],
+        imgs: ["./img/hoge.jpg","hoge"],
+        topimgurl: "./img/hoge.jpg",
+        topvideourl: ""
+    }
+}
+
+imgsは下に配置するイメージたち。
+トップがイメージならtopimgurlはイメージのパス、そうでなければ""でvideoの埋め込みurlを入れる。
+
+vcaptionは必要であれば。他は""で空を入れておくのが吉。
+
+const _textdata = {
+    th_hoge: {
+        ja: `hoge
+        2020`,
+        en: `hoge
+        2020`
+    },
+    title_hoge: {
+        ja: `hoge`,
+        en: `hoge`
+    },
+    credit_hoge: {
+        ja: ``,
+        en: ``
+    },
+    link_hoge: {
+        ja: ">>Link",
+        en: ">>Link"
+    },
+    text_hoge: {
+        ja: ``,
+        en: ``
+    },
+    vcaption_hoge: {
+        ja: ``,
+        en: ``
+    }
+}
+
+*/
+
+
+const _thumbnaildata = [
+    {
+        root_id: "portfolio",
+        img_url: "./img/thumbnails/t_portfolio.jpg"
     },
     {
-        root_id: "testid2",
-        img_url: "./img/exhidm.jpg",
-        title_id: "title2"
+        root_id: "cisyn",
+        img_url: "./img/thumbnails/t_cisyn.jpg"
     },
     {
-        root_id: "testid3",
-        img_url: "./img/exhidm.jpg",
-        title_id: "title3"
+        root_id: "getclosemore",
+        img_url: "./img/thumbnails/t_getclosemore.jpg"
+    },
+    {
+        root_id: "lumiere",
+        img_url: "./img/thumbnails/t_lumiere.jpg"
+    },
+    {
+        root_id: "fts_photo",
+        img_url: "./img/thumbnails/t_fts_photo.jpg"
+    },
+    {
+        root_id: "koko",
+        img_url: "./img/thumbnails/t_koko.jpg"
+    },
+    {
+        root_id: "isyourbodyyours",
+        img_url: "./img/thumbnails/t_isyourbodyyours.jpg"
+    },
+    {
+        root_id: "selfportrait",
+        img_url: "./img/thumbnails/t_selfportrait.jpg"
+    },
+    {
+        root_id: "practicetowalk",
+        img_url: "./img/thumbnails/t_practicetowalk.jpg"
+    },
+    {
+        root_id: "sowhoami",
+        img_url: "./img/thumbnails/t_sowhoami.jpg"
+    },
+    {
+        root_id: "eupcaccia",
+        img_url: "./img/thumbnails/t_eupcaccia.jpg"
+    },
+    {
+        root_id: "osorubeki",
+        img_url: "./img/thumbnails/t_osorubeki.jpg"
+    },
+    {
+        root_id: "japanesefaces",
+        img_url: "./img/thumbnails/t_japanesefaces.jpg"
+    },
+    {
+        root_id: "secretplace",
+        img_url: "./img/thumbnails/t_secretplace.jpg"
+    },
+    {
+        root_id: "omatsurisawagi",
+        img_url: "./img/thumbnails/t_omatsurisawagi.jpg"
     }
 ]
 
 const _documentdata = {
     //keyネームは_thumbnaildataのroot_id
-    testid1: {
-        titleid: "doctitle1",
-        creditid: "doccredit1",
-        links: [{ id: "doclink1_1", url: "" }],
-        descriptionid: "doctext1",
-        imgs: ["./img/exhibs.jpg"],
-        topimg: "",
-        topvideourl: "",
-        topvideocaption: ""
+    //他の要素: title_hoge, credit_hoge, text_hoge, vcaption_hoge
+    //hogeにはkeyが入る。 textの方のデータに入れてくのでお忘れなく。
+    portfolio: {
+        links: [{ id: "link_portfolio", url: "./data/portfolio.pdf" }],
+        imgs: [],
+        topimgurl: "./img/portfolio_top.jpg",
+        topvideourl: ""
     },
-    testid2: {
-        titleid: "doctitle2",
-        creditid: "doccredit2",
-        links: [{ id: "doclink1_2", url: "" }],
-        descriptionid: "doctext2",
-        imgs: ["./img/exhidm.jpg"],
-        topimg: "",
-        topvideourl: "",
-        topvideocaption: ""
+    cisyn: {
+        links: [{ id: "link_cisyn", url: "https://caniseeyounow.tokyo/" }],
+        imgs: ["./img/cisyn/img1.jpg", "./img/cisyn/img5.jpg", "./img/cisyn/img3.jpg"],
+        topimgurl: "",
+        topvideourl: "https://www.youtube.com/embed/9RZtiaJZQzg"
+    },
+    getclosemore: {
+        links: [],
+        imgs: ["./img/getclosemore/img1.jpg", "./img/getclosemore/img2.jpg", "./img/getclosemore/img3.jpg"],
+        topimgurl: "",
+        topvideourl: "https://www.youtube.com/embed/w7lutZxABxY"
+    },
+    lumiere: {
+        links: [],
+        imgs: ["./img/lumiere/img1.jpg", "./img/lumiere/img3.jpg", "./img/lumiere/img4.jpg"],
+        topimgurl: "",
+        topvideourl: "https://www.youtube.com/embed/10FNJ1RaABw"
+    },
+    fts_photo: {
+        links: [{ id: "link_fts_photo", url: "hoge" }],
+        imgs: ["./img/fts_photo/img2.jpg", "./img/fts_photo/img3.jpg", "./img/fts_photo/img4.jpg", "./img/fts_photo/img5.jpg", "./img/fts_photo/img6.jpg", "./img/fts_photo/img7.jpg", "./img/fts_photo/img8.jpg", "./img/fts_photo/img9.jpg"],
+        topimgurl: "./img/fts_photo/img1.jpg",
+        topvideourl: ""
+    },
+    koko: {
+        links: [],
+        imgs: ["./img/koko/img1.jpg"],
+        topimgurl: "",
+        topvideourl: "https://www.youtube.com/embed/KA5s8VarmSg"
+    },
+    isyourbodyyours: {
+        links: [],
+        imgs: ["./img/isyourbodyyours/img1.jpg", "./img/isyourbodyyours/img4.jpg", "./img/isyourbodyyours/img3.jpg", "./img/isyourbodyyours/img6.jpg", "./img/isyourbodyyours/img5.jpg", "./img/isyourbodyyours/img8.jpg", "./img/isyourbodyyours/img10.jpg"],
+        topimgurl: "",
+        topvideourl: "https://www.youtube.com/embed/bchO62Ej2Nw"
+    },
+    selfportrait: {
+        links: [],
+        imgs: ["./img/selfportrait/img2.jpg", "./img/selfportrait/img3.jpg"],
+        topimgurl: "./img/selfportrait/img1.jpg",
+        topvideourl: ""
+    },
+    practicetowalk: {
+        links: [],
+        imgs: ["./img/practicetowalk/img1.jpg", "./img/practicetowalk/img3.jpg", "./img/practicetowalk/img5.jpg", "./img/practicetowalk/img6.jpg", "./img/practicetowalk/img7.jpg"],
+        topimgurl: "",
+        topvideourl: "https://www.youtube.com/embed/KBaWHcB7fEA"
+    },
+    sowhoami: {
+        links: [],
+        imgs: ["./img/sowhoami/img1.jpg", "./img/sowhoami/img2.jpg", "./img/sowhoami/img3.jpg", "./img/sowhoami/img5.jpg"],
+        topimgurl: "",
+        topvideourl: "https://www.youtube.com/embed/v8dlxSbg-Yk"
+    },
+    eupcaccia: {
+        links: [],
+        imgs: ["./img/eupcaccia/img5.jpg", "./img/eupcaccia/img1.jpg", "./img/eupcaccia/img6.jpg", "./img/eupcaccia/img7.jpg"],
+        topimgurl: "",
+        topvideourl: "https://www.youtube.com/embed/bTBIAVwz2fY"
+    },
+    osorubeki: {
+        links: [],
+        imgs: ["./img/osorubeki/img2.jpg", "./img/osorubeki/img3.jpg", "./img/osorubeki/img4.jpg", "./img/osorubeki/img5.jpg"],
+        topimgurl: "./img/osorubeki/img1.jpg",
+        topvideourl: ""
+    },
+    japanesefaces: {
+        links: [],
+        imgs: ["./img/japanesefaces/img2.jpg", "./img/japanesefaces/img3.jpg", "./img/japanesefaces/img4.jpg", "./img/japanesefaces/img6.jpg", "./img/japanesefaces/img7.jpg", "./img/japanesefaces/img8.jpg", "./img/japanesefaces/img9.jpg"],
+        topimgurl: "./img/japanesefaces/img1.jpg",
+        topvideourl: ""
+    },
+    secretplace: {
+        links: [],
+        imgs: ["./img/secretplace/img2.jpg", "./img/secretplace/img3.jpg", "./img/secretplace/img4.jpg", "./img/secretplace/img8.jpg", "./img/secretplace/img5.jpg", "./img/secretplace/img7.jpg"],
+        topimgurl: "./img/secretplace/img1.jpg",
+        topvideourl: ""
+    },
+    omatsurisawagi: {
+        links: [],
+        imgs: [],
+        topimgurl: "./img/omatsurisawagi/img1.jpg",
+        topvideourl: ""
     }
+
+
 }
 
 const _textdata = {
-    title1: {
-        ja: `タイトルサンプル
-        aaa`,
-        en: `title sample`
+    th_portfolio: {
+        ja: `PORTFOLIO
+        2020`,
+        en: `PORTFOLIO
+        2020`
     },
-    doctitle1: {
-        ja: "aaaa",
-        en: ""
+    title_portfolio: {
+        ja: `PORTFOLIO`,
+        en: `PORTFOLIO`
     },
-    doccredit1: {
+    credit_portfolio: {
         ja: ``,
         en: ``
     },
-    doclink1_1: {
-        ja: "aaaaa",
-        en: ""
+    link_portfolio: {
+        ja: ">>Link",
+        en: ">>Link"
     },
-    doctext1: {
-        ja: "",
-        en: ""
+    text_portfolio: {
+        ja: ``,
+        en: ``
+    },
+    th_cisyn: {
+        ja: `Can I see You now?
+        online app / 2020`,
+        en: `hoge
+        2020`
+    },
+    title_cisyn: {
+        ja: `Can I see You now?`,
+        en: `Can I see You now?`
+    },
+    credit_cisyn: {
+        ja: `Artist : Yuto Hayashi
+Engineer : Tabi Morinaga
+2020 / online application
+関連作品: “I want to get close more...”`,
+        en: `Artist : Yuto Hayashi
+Engineer : Tabi Morinaga
+2020 / online application
+Related work: “I want to get close more...”`
+    },
+    link_cisyn: {
+        ja: ">>特設サイトリンク",
+        en: ">>Specialised website"
+    },
+    text_cisyn: {
+        ja: `前作“I want to get close more...”プロジェクトの発展として、オンラインプレイ可能なPCアプリケーションを設計しました。
+2020年、人々は物理的な距離によって強烈に隔てられました。今、私たちはどうやって人に会うことができるのでしょう？`,
+        en: `This piece is a progress of the previous work, “I want to get close more...”.
+In 2020, people were strongly separated by physical distance. How can we meet people now? As an attempt this time, I designed a session.
+*This work is a PC application that is played online.
+`
+    },
+    vcaption_cisyn: {
+        ja: `Trailer`,
+        en: `Trailer`
     }
 }
--- docs/project/renewal/js/works_script.js
+++ docs/project/renewal/js/works_script.js
@@ -33,57 +33,60 @@ function setthumbnail(targetelement, thumbnaildata) {
         var content = document.querySelector("#thumbnail_template").content;
         var clone = document.importNode(content, true);
         var root = clone.querySelector(".thumbnail_box");
-        //
-        // root.setAttribute("data-target", thumbnaildata.)
+
         root.id = thumbnaildata[i].root_id
         var thumbnail = clone.querySelector(".thumbnail");
         thumbnail.style.backgroundImage = "url('" + thumbnaildata[i].img_url + "')";
         var title = clone.querySelector(".thumbnail_title");
-        title.id = thumbnaildata[i].title_id;
-
+        title.id = "th_" + thumbnaildata[i].root_id;
         targetelement.appendChild(clone);
     }
 }
 
-function setvideo(targetelement, captiontext, targeturl) {
+function setvideo(targetelement, captiontextid, targeturl) {
     var content = document.querySelector("#video_template").content;
     var clone = document.importNode(content, true);
     var caption = clone.querySelector(".video_caption");
-    caption.textContent = captiontext;
+    caption.id = captiontextid;
     var youtubeurl = clone.querySelector("#youtubeiframe");
     youtubeurl.src = targeturl;
     targetelement.appendChild(clone);
 }
 
 function setdocument(documentdata_key) {
+    //_documentdata オブジェクトのキーを参照している。キーをidとしても使っているので、アクセスしやすく、重複して同じものを書いたりしなくて済むようデータ構造を工夫しているつもり。
     var documentdata = _documentdata[documentdata_key];
     var content = document.querySelector("#document_template").content;
     var clone = document.importNode(content, true);
     var root = clone.querySelector(".document_wrapper");
     root.id = documentdata_key;
-    var topmedia = clone.querySelector(".work_title");
+    var topmedia = clone.querySelector(".document_topmedia");
     if (documentdata.topimgurl != "") {
         // topmedia
         var img = document.createElement("img");
+        img.src = documentdata.topimgurl;
         topmedia.appendChild(img);
+        console.log(documentdata.topimgurl);
     } else {
-        setvideo(topmedia, documentdata.topvideocaption, documentdata.topvideourl);
+        var topvideocaptionid = "vcaption_" + documentdata_key;
+        setvideo(topmedia, topvideocaptionid, documentdata.topvideourl);
     }
     var title = clone.querySelector(".work_title");
-    title.id = documentdata.titleid;
+    title.id = "title_" + documentdata_key;
     var credit = clone.querySelector(".work_credit");
-    credit.id = documentdata.creditid;
+    credit.id = "credit_" + documentdata_key;
     //複数回回す
     var link = clone.querySelector(".work_link");
     for (var i = 0; i < documentdata.links.length; i++) {
         var a = document.createElement("a");
         a.href = documentdata.links[i].url;
         a.id = documentdata.links[i].id;
+        a.target = "_blank";
         link.appendChild(a);
         link.appendChild(document.createElement("br"));
     }
     var description = clone.querySelector(".work_description");
-    description.id = documentdata.descriptionid;
+    description.id = "text_" + documentdata_key;
     var imgbox = clone.querySelector(".work_imgbox");
     for (var i = 0; i < documentdata.imgs.length; i++) {
         // console.log(documentdata.imgs[i]);
@@ -93,6 +96,7 @@ function setdocument(documentdata_key) {
         imgbox.appendChild(img);
     }
     $('#' + documentdata_key).after(clone) + '"';
+    //フェードインをhideをはさむだけのお手軽実装。hideは隠してるだけで要素の検索とかは同じフレーム内でも問題なさそう。
     $(".document_wrapper").hide().fadeIn(400);
     settext();
 }
@@ -102,10 +106,11 @@ function button_thumbnail(targetid) {
     var activecontent = document.getElementsByClassName("document_wrapper")[0];
     var issame = false;
     if (activecontent != null) {
-        console.log(activecontent);
+        // console.log(activecontent);
         if (activecontent.id == targetid) {
             issame = true;
         }
+        //フェードアウト。ディレイ処理で完全に消すようにしてるけど、もしかしていらない？
         $(activecontent).fadeOut(200);
         setTimeout(function () {
             // console.log("null");
