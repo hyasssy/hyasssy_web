b5e0a5859e8c42c83a9c367b96c6df9ed91829b3
Yuto Hayashi <hyasssy@gmail.com>
2021-06-13 23:35:04 +0900

    やっぱりworksをトップページにした

--- docs/works/works_data.js
+++ docs/works/works_data.js
@@ -4,15 +4,15 @@ Format
 const _thumbnaildata = [
     {
         root_id: "hoge",
-        img_url: "../img/thumbnails/t_hoge.jpg"
+        img_url: "./img/thumbnails/t_hoge.jpg"
     }
 ]
 const _documentdata = {
     //ここのキーは_thumbnaildataのroot_idと同じにする。
     hoge: {
         links: [{ id: "link_hoge", url: "hoge" }],
-        imgs: ["../img/hoge.jpg","hoge"],
-        topimgurl: "../img/hoge.jpg",
+        imgs: ["./img/hoge.jpg","hoge"],
+        topimgurl: "./img/hoge.jpg",
         topvideourl: ""
     }
 }
@@ -57,87 +57,87 @@ const _textdata = {
 const _thumbnaildata = [//ここの並び順で並んでいく。
     {
         root_id: "fragile",
-        img_url: "../img/thumbnails/t_fragile.jpg"
+        img_url: "./img/thumbnails/t_fragile.jpg"
     },
     {
         root_id: "cisyn",
-        img_url: "../img/thumbnails/t_cisyn.jpg"
+        img_url: "./img/thumbnails/t_cisyn.jpg"
     },
     {
         root_id: "web_ga",
-        img_url: "../img/thumbnails/t_web_ga.jpg"
+        img_url: "./img/thumbnails/t_web_ga.jpg"
     },
     {
         root_id: "web_okuyama",
-        img_url: "../img/thumbnails/t_web_okuyama.jpg"
+        img_url: "./img/thumbnails/t_web_okuyama.jpg"
     },
     {
         root_id: "web_mp",
-        img_url: "../img/thumbnails/t_web_mp.jpg"
+        img_url: "./img/thumbnails/t_web_mp.jpg"
     },
     {
         root_id: "getclosemore",
-        img_url: "../img/thumbnails/t_getclosemore.jpg"
+        img_url: "./img/thumbnails/t_getclosemore.jpg"
     },
     {
         root_id: "lumiere",
-        img_url: "../img/thumbnails/t_lumiere.jpg"
+        img_url: "./img/thumbnails/t_lumiere.jpg"
     },
     {
         root_id: "fts_photo",
-        img_url: "../img/thumbnails/t_fts_photo.jpg"
+        img_url: "./img/thumbnails/t_fts_photo.jpg"
     },
     {
         root_id: "koko",
-        img_url: "../img/thumbnails/t_koko.jpg"
+        img_url: "./img/thumbnails/t_koko.jpg"
     },
     {
         root_id: "portfolio",
-        img_url: "../img/thumbnails/t_portfolio.jpg"
+        img_url: "./img/thumbnails/t_portfolio.jpg"
     },
     {
         root_id: "isyourbodyyours",
-        img_url: "../img/thumbnails/t_isyourbodyyours.jpg"
+        img_url: "./img/thumbnails/t_isyourbodyyours.jpg"
     },
     {
         root_id: "selfportrait",
-        img_url: "../img/thumbnails/t_selfportrait.jpg"
+        img_url: "./img/thumbnails/t_selfportrait.jpg"
     },
     {
         root_id: "practicetowalk",
-        img_url: "../img/thumbnails/t_practicetowalk.jpg"
+        img_url: "./img/thumbnails/t_practicetowalk.jpg"
     },
     {
         root_id: "fts_video",
-        img_url: "../img/thumbnails/t_fts_video.jpg"
+        img_url: "./img/thumbnails/t_fts_video.jpg"
     },
     {
         root_id: "sowhoami",
-        img_url: "../img/thumbnails/t_sowhoami.jpg"
+        img_url: "./img/thumbnails/t_sowhoami.jpg"
     },
     {
         root_id: "eupcaccia",
-        img_url: "../img/thumbnails/t_eupcaccia.jpg"
+        img_url: "./img/thumbnails/t_eupcaccia.jpg"
     },
     {
         root_id: "osorubeki",
-        img_url: "../img/thumbnails/t_osorubeki.jpg"
+        img_url: "./img/thumbnails/t_osorubeki.jpg"
     },
     {
         root_id: "japanesefaces",
-        img_url: "../img/thumbnails/t_japanesefaces.jpg"
+        img_url: "./img/thumbnails/t_japanesefaces.jpg"
     },
     {
         root_id: "secretplace",
-        img_url: "../img/thumbnails/t_secretplace.jpg"
+        img_url: "./img/thumbnails/t_secretplace.jpg"
     },
     {
         root_id: "flatworks",
-        img_url: "../img/thumbnails/t_flatworks.jpg"
+        img_url: "./img/thumbnails/t_flatworks.jpg"
     },
     {
         root_id: "omatsurisawagi",
-        img_url: "../img/thumbnails/t_omatsurisawagi.jpg"
+        img_url: "./img/thumbnails/t_omatsurisawagi.jpg"
     }
 ]
 
@@ -146,129 +146,129 @@ const _documentdata = {
     //他の要素: title_hoge, credit_hoge, text_hoge, vcaption_hoge
     //hogeにはkeyが入る。 textの方のデータに入れてくのでお忘れなく。
     portfolio: {
-        links: [{ id: "link_portfolio", url: "../img/portfolio/portfolio.pdf" }],
+        links: [{ id: "link_portfolio", url: "./img/portfolio/portfolio.pdf" }],
         imgs: [],
-        topimgurl: "../img/portfolio/portfolio_top.jpg",
+        topimgurl: "./img/portfolio/portfolio_top.jpg",
         topvideourl: ""
     },
     cisyn: {
         links: [{ id: "link_cisyn", url: "https://caniseeyounow.tokyo/" }],
-        imgs: ["../img/cisyn/img1.jpg", "../img/cisyn/img5.jpg", "../img/cisyn/img3.jpg"],
+        imgs: ["./img/cisyn/img1.jpg", "./img/cisyn/img5.jpg", "./img/cisyn/img3.jpg"],
         topimgurl: "",
         topvideourl: "https://www.youtube.com/embed/9RZtiaJZQzg"
     },
     fragile: {
         links: [{ id: "link_fragile", url: "https://hyasssy.tokyo/project/fragile/" }],
-        imgs: ["../img/fragile/img2.jpg", "../img/fragile/img4.jpg", "../img/fragile/img7.jpg", "../img/fragile/img6.jpg", "../img/fragile/img9.jpg"],
+        imgs: ["./img/fragile/img2.jpg", "./img/fragile/img4.jpg", "./img/fragile/img7.jpg", "./img/fragile/img6.jpg", "./img/fragile/img9.jpg"],
         topimgurl: "",
         topvideourl: "https://www.youtube.com/embed/bkVomLX833M"
     },
     web_ga: {
         links: [{ id: "link_web_ga", url: "https://welcome.geidai.ac.jp/" }],
-        imgs: ["../img/web_ga/img2.jpg", "../img/web_ga/img3.jpg", "../img/web_ga/img4.jpg", "../img/web_ga/virtualtour.gif"],
-        topimgurl: "../img/web_ga/img1.jpg",
+        imgs: ["./img/web_ga/img2.jpg", "./img/web_ga/img3.jpg", "./img/web_ga/img4.jpg", "./img/web_ga/virtualtour.gif"],
+        topimgurl: "./img/web_ga/img1.jpg",
         topvideourl: ""
     },
     web_okuyama: {
         links: [{ id: "link_web_okuyama", url: "https://honoka-okuyama.com/" }],
-        imgs: ["../img/web_okuyama/img2.jpg", "../img/web_okuyama/img3.jpg"],
-        topimgurl: "../img/web_okuyama/img1.jpg",
+        imgs: ["./img/web_okuyama/img2.jpg", "./img/web_okuyama/img3.jpg"],
+        topimgurl: "./img/web_okuyama/img1.jpg",
         topvideourl: ""
     },
     web_mp: {
         links: [{ id: "link_web_mp", url: "https://fm.geidai.ac.jp/media-practice/2021/" }],
-        imgs: ["../img/web_mp/img2.jpg", "../img/web_mp/img3.jpg", "../img/web_mp/img4.jpg", "../img/web_mp/img5.jpg"],
-        topimgurl: "../img/web_mp/img1.jpg",
+        imgs: ["./img/web_mp/img2.jpg", "./img/web_mp/img3.jpg", "./img/web_mp/img4.jpg", "./img/web_mp/img5.jpg"],
+        topimgurl: "./img/web_mp/img1.jpg",
         topvideourl: ""
     },
     getclosemore: {
         links: [],
-        imgs: ["../img/getclosemore/img1.jpg", "../img/getclosemore/img2.jpg", "../img/getclosemore/img3.jpg"],
+        imgs: ["./img/getclosemore/img1.jpg", "./img/getclosemore/img2.jpg", "./img/getclosemore/img3.jpg"],
         topimgurl: "",
         topvideourl: "https://www.youtube.com/embed/w7lutZxABxY"
     },
     lumiere: {
         links: [],
-        imgs: ["../img/lumiere/img1.jpg", "../img/lumiere/img3.jpg", "../img/lumiere/img4.jpg"],
+        imgs: ["./img/lumiere/img1.jpg", "./img/lumiere/img3.jpg", "./img/lumiere/img4.jpg"],
         topimgurl: "",
         topvideourl: "https://www.youtube.com/embed/10FNJ1RaABw"
     },
     fts_photo: {
         links: [],
-        imgs: ["../img/fts_photo/img2.jpg", "../img/fts_photo/img3.jpg", "../img/fts_photo/img4.jpg", "../img/fts_photo/img5.jpg", "../img/fts_photo/img6.jpg", "../img/fts_photo/img7.jpg", "../img/fts_photo/img8.jpg", "../img/fts_photo/img9.jpg"],
-        topimgurl: "../img/fts_photo/img1.jpg",
+        imgs: ["./img/fts_photo/img2.jpg", "./img/fts_photo/img3.jpg", "./img/fts_photo/img4.jpg", "./img/fts_photo/img5.jpg", "./img/fts_photo/img6.jpg", "./img/fts_photo/img7.jpg", "./img/fts_photo/img8.jpg", "./img/fts_photo/img9.jpg"],
+        topimgurl: "./img/fts_photo/img1.jpg",
         topvideourl: ""
     },
     koko: {
         links: [],
-        imgs: ["../img/koko/img1.jpg"],
+        imgs: ["./img/koko/img1.jpg"],
         topimgurl: "",
         topvideourl: "https://www.youtube.com/embed/KA5s8VarmSg"
     },
     isyourbodyyours: {
         links: [],
-        imgs: ["../img/isyourbodyyours/img1.jpg", "../img/isyourbodyyours/img4.jpg", "../img/isyourbodyyours/img3.jpg", "../img/isyourbodyyours/img6.jpg", "../img/isyourbodyyours/img5.jpg", "../img/isyourbodyyours/img8.jpg", "../img/isyourbodyyours/img10.jpg"],
+        imgs: ["./img/isyourbodyyours/img1.jpg", "./img/isyourbodyyours/img4.jpg", "./img/isyourbodyyours/img3.jpg", "./img/isyourbodyyours/img6.jpg", "./img/isyourbodyyours/img5.jpg", "./img/isyourbodyyours/img8.jpg", "./img/isyourbodyyours/img10.jpg"],
         topimgurl: "",
         topvideourl: "https://www.youtube.com/embed/bchO62Ej2Nw"
     },
     selfportrait: {
         links: [],
-        imgs: ["../img/selfportrait/img2.jpg", "../img/selfportrait/img3.jpg"],
-        topimgurl: "../img/selfportrait/img1.jpg",
+        imgs: ["./img/selfportrait/img2.jpg", "./img/selfportrait/img3.jpg"],
+        topimgurl: "./img/selfportrait/img1.jpg",
         topvideourl: ""
     },
     practicetowalk: {
         links: [],
-        imgs: ["../img/practicetowalk/img1.jpg", "../img/practicetowalk/img3.jpg", "../img/practicetowalk/img5.jpg", "../img/practicetowalk/img6.jpg", "../img/practicetowalk/img7.jpg"],
+        imgs: ["./img/practicetowalk/img1.jpg", "./img/practicetowalk/img3.jpg", "./img/practicetowalk/img5.jpg", "./img/practicetowalk/img6.jpg", "./img/practicetowalk/img7.jpg"],
         topimgurl: "",
         topvideourl: "https://www.youtube.com/embed/KBaWHcB7fEA"
     },
     fts_video: {
         links: [],
-        imgs: ["../img/fts_video/img6.jpg", "../img/fts_video/img7.jpg", "../img/fts_video/img8.jpg", "../img/fts_video/img3.jpg", "../img/fts_video/img4.jpg"],
+        imgs: ["./img/fts_video/img6.jpg", "./img/fts_video/img7.jpg", "./img/fts_video/img8.jpg", "./img/fts_video/img3.jpg", "./img/fts_video/img4.jpg"],
         topimgurl: "",
         topvideourl: "https://www.youtube.com/embed/jcxe5_TK_Wo"
     },
     sowhoami: {
         links: [],
-        imgs: ["../img/sowhoami/img1.jpg", "../img/sowhoami/img2.jpg", "../img/sowhoami/img3.jpg", "../img/sowhoami/img5.jpg"],
+        imgs: ["./img/sowhoami/img1.jpg", "./img/sowhoami/img2.jpg", "./img/sowhoami/img3.jpg", "./img/sowhoami/img5.jpg"],
         topimgurl: "",
         topvideourl: "https://www.youtube.com/embed/v8dlxSbg-Yk"
     },
     eupcaccia: {
         links: [],
-        imgs: ["../img/eupcaccia/img5.jpg", "../img/eupcaccia/img1.jpg", "../img/eupcaccia/img6.jpg", "../img/eupcaccia/img7.jpg"],
+        imgs: ["./img/eupcaccia/img5.jpg", "./img/eupcaccia/img1.jpg", "./img/eupcaccia/img6.jpg", "./img/eupcaccia/img7.jpg"],
         topimgurl: "",
         topvideourl: "https://www.youtube.com/embed/bTBIAVwz2fY"
     },
     osorubeki: {
         links: [],
-        imgs: ["../img/osorubeki/img2.jpg", "../img/osorubeki/img3.jpg", "../img/osorubeki/img4.jpg", "../img/osorubeki/img5.jpg"],
-        topimgurl: "../img/osorubeki/img1.jpg",
+        imgs: ["./img/osorubeki/img2.jpg", "./img/osorubeki/img3.jpg", "./img/osorubeki/img4.jpg", "./img/osorubeki/img5.jpg"],
+        topimgurl: "./img/osorubeki/img1.jpg",
         topvideourl: ""
     },
     japanesefaces: {
         links: [],
-        imgs: ["../img/japanesefaces/img2.jpg", "../img/japanesefaces/img3.jpg", "../img/japanesefaces/img4.jpg", "../img/japanesefaces/img6.jpg", "../img/japanesefaces/img7.jpg", "../img/japanesefaces/img8.jpg", "../img/japanesefaces/img9.jpg"],
-        topimgurl: "../img/japanesefaces/img1.jpg",
+        imgs: ["./img/japanesefaces/img2.jpg", "./img/japanesefaces/img3.jpg", "./img/japanesefaces/img4.jpg", "./img/japanesefaces/img6.jpg", "./img/japanesefaces/img7.jpg", "./img/japanesefaces/img8.jpg", "./img/japanesefaces/img9.jpg"],
+        topimgurl: "./img/japanesefaces/img1.jpg",
         topvideourl: ""
     },
     secretplace: {
         links: [],
-        imgs: ["../img/secretplace/img2.jpg", "../img/secretplace/img3.jpg", "../img/secretplace/img4.jpg", "../img/secretplace/img8.jpg", "../img/secretplace/img5.jpg", "../img/secretplace/img7.jpg"],
-        topimgurl: "../img/secretplace/img1.jpg",
+        imgs: ["./img/secretplace/img2.jpg", "./img/secretplace/img3.jpg", "./img/secretplace/img4.jpg", "./img/secretplace/img8.jpg", "./img/secretplace/img5.jpg", "./img/secretplace/img7.jpg"],
+        topimgurl: "./img/secretplace/img1.jpg",
         topvideourl: ""
     },
     flatworks: {
         links: [],
-        imgs: ["../img/flatworks/img2.jpg", "../img/flatworks/img3.jpg", "../img/flatworks/img4.jpg", "../img/flatworks/img5.jpg"],
-        topimgurl: "../img/flatworks/img1.jpg",
+        imgs: ["./img/flatworks/img2.jpg", "./img/flatworks/img3.jpg", "./img/flatworks/img4.jpg", "./img/flatworks/img5.jpg"],
+        topimgurl: "./img/flatworks/img1.jpg",
         topvideourl: ""
     },
     omatsurisawagi: {
         links: [],
         imgs: [],
-        topimgurl: "../img/omatsurisawagi/img1.jpg",
+        topimgurl: "./img/omatsurisawagi/img1.jpg",
         topvideourl: ""
     }
 
